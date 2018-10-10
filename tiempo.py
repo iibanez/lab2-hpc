f06d7aaaaec754129f551fa7e7a934b05bc1faf9
Ignacio Ibanez <ignacio.ibanez@usach.cl>
2018-10-10 14:41:36 -0300

    ejecucion del programa para calcular tiempos de ejecucion

---
+++ tiempo.py
@@ -0,0 +1,17 @@
+import commands
+
+archivo = open("tiempos.txt","w")
+
+for i in range(1,25):
+
+	suma = 0
+	for j in range(3):
+		comando = "time ./build/mandelbrotp -i 500 -a -1 -b -1 -c 1 -d 1 -s 0.001 -f salida3.raw -t "+str(i)
+		resultado = commands.getoutput(comando)
+		separado = resultado.split("\t")
+		suma += float(separado[1][2:7])
+	
+	suma = suma/3
+	archivo.write(str(suma)+"\n")
+
+archivo.close()
